dac55137c1bc0b2ccf294268fabcc8172c974d6d
geardyy22 <degraciakellyjay@gmail.com>
2024-10-25 01:20:34 +0800

    Login

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css'; // Assuming you have some basic styling in App.css
 
-const Login = () => {   
+const login = () => {
   return (
-    <>
-      <div>
-
+    <div className="App">
+      <header className="App-header">
+        <img src="path_to_your_image.jpg" className="App-image" alt="rice plants" />
+      </header>
+      <div className="login-container">
+        <h2>Sign In</h2>
+        <p>Log in to your account</p>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">
+              <i className="fa fa-envelope"></i>
+              <input type="email" id="email" placeholder="example@email.com" required />
+            </label>
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">
+              <i className="fa fa-lock"></i>
+              <input type="password" id="password" placeholder="Password" required />
+            </label>
+          </div>
+          <div className="options">
+            <label>
+              <input type="checkbox" />
+              Remember me
+            </label>
+            <a href="/forgot-password">Forgot Password?</a>
+          </div>
+          <button type="submit" className="login-button">Log In</button>
+        </form>
+        <p>Don't have an account? <a href="/sign-up">Sign Up</a></p>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Login
+export default login;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/bootstrap.min.css';
+import login from './Pages/login.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <login />
   </StrictMode>,
 )
